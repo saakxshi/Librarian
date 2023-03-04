4959bdb8813804dd4242c82f96686883706dfcfd
saakxshi <saakshijha23@gmail.com>
2023-03-04 14:38:23 +0530

    Added logo and login button

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,35 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+<v-app>
+    <v-app-bar app color="primary" dark>
+        <div class="d-flex align-center">
+            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="@/assets/logo.png" transition="scale-transition" width="40" />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+            <p>{{ $const.APP_NAME}}</p>
+        </div>
 
-      <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-btn text>
+            Login
+        </v-btn>
+        <v-btn text>
+            <v-icon>mdi-account-circle</v-icon>
+        </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+        <router-view />
     </v-main>
-  </v-app>
+</v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+    name: 'App',
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        //
+    }),
 };
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
-  <hello-world />
+  <div :style="`background-color: ${randomColor()}`">
+    <h1>Welcome to Librarian</h1>
+  </div>
+  
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+      
     },
   }
 </script>
