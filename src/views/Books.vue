33423b4a988fe66916030cd8c1f4291a4ea9be3c
saakxshi <saakshijha23@gmail.com>
2023-04-01 01:08:08 +0530

    Added Pages and fuctionality

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,10 +1,10 @@
 <template>
 <div>
 
-    <AppHeader title="Books"></AppHeader>
+    <AppHeader title="Books" @add="$router.push('/')" @search="searchInBooks($event)"></AppHeader>
     <v-list>
 
-        <v-list-item v-for="(item,i) in books" :key="i">
+        <v-list-item v-for=" (item,i) in books" :key="i">
             <v-list-item-avatar tile>
                 <v-img :src="item.img"></v-img>
             </v-list-item-avatar>
@@ -24,21 +24,21 @@
                             </v-btn>
                         </template>
                         <v-list>
-                            <v-list-item>
+                            <v-list-item @click="$router.push('/books/'+item._id)">
                                 <v-list-item-title>Edit</v-list-item-title>
                                 <v-icon>mdi-pencil</v-icon>
                             </v-list-item>
-                            <v-list-item>
+                            <v-list-item @click="$emit('delete')">
                                 <v-list-item-title>Delete</v-list-item-title>
                                 <v-icon>mdi-delete</v-icon>
 
                             </v-list-item>
-                            <v-list-item>
+                            <v-list-item @click="$emit('')">
                                 <v-list-item-title>Issue</v-list-item-title>
                                 <v-icon>mdi-book-arrow-right</v-icon>
 
                             </v-list-item>
-                            <v-list-item>
+                            <v-list-item @click="$emit('')">
                                 <v-list-item-title>Return</v-list-item-title>
                                 <v-icon>mdi-book-arrow-left</v-icon>
 
@@ -66,6 +66,14 @@ export default {
 
     components: {
         AppHeader,
+    },
+    methods: {
+        searchInBooks(searchText) {
+            if (searchText) {
+                this.books = BooksList.filter(rec => rec.title.toLowerCase().includes(searchText.toLowerCase()))
+            } else
+                this.books = BooksList
+        }
     },
 
 }
